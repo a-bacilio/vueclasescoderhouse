fa780c62286641253ce49c3b0e1fbf653a714703
Anthony Bacilio Ruiz <bacilio.anthony@gmail.com>
2022-11-21 19:28:30 -0500

    tareita de clase 3

--- js/app.js
+++ js/app.js
@@ -2,6 +2,17 @@ const app = new Vue({
   el: "#app",
   data: {
     usuario: "Alberto",
+    derecho: true,
+    izquierdo: true,
   },
-  methods: {},
+  methods: {
+    presionaIzquierdo() {
+      this.derecho = true;
+      this.izquierdo = false;
+    },
+    presionaDerecho() {
+      this.derecho = false;
+      this.izquierdo = true;
+    },
+  },
 });
